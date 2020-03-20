90a2cd99bd9de7a29f12cd875bb724aaf418d193
powellsl99 <powellsl99@gmail.com>
2020-03-20 19:03:37 -0400

    resolve merge

--- src/components/home.js
+++ src/components/home.js
@@ -14,19 +14,3 @@ export default function TOS() {
     </Container>
   );
 }
-=======
-import React from "react";
-import Grid from "@material-ui/core/Grid";
-import Container from "@material-ui/core/Container";
-import Link from "@material-ui/core/Link";
-import Typography from "@material-ui/core/Typography";
-
-export default function TOS() {
-  return (
-    <Container component="main" maxWidth="xs" align="center">
-      <Typography component="h1" variant="h5" align="center">
-        Welcome to ReachOut
-      </Typography>
-    </Container>
-  );
-}
