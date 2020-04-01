7eb6adbb74c49103806582a6453bda85c3f04a9d
Preetham-Syamala <33362250+Preetham-Syamala@users.noreply.github.com>
2020-04-01 18:13:35 -0400

    Home layout
    wrapped home page in container to fix layout issues

--- src/components/appts.js
+++ src/components/appts.js
@@ -13,21 +13,10 @@ import appointments from './appointments.json'
 
 const useStyles = makeStyles({
   table: {
-    //minWidth: 650,
+    fontSize: 30
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
 
 const MyApp = () => {
 
@@ -46,9 +35,9 @@ const MyApp = () => {
               <TableCell align="left"><strong>Actions</strong></TableCell>
             </TableRow>
           </TableHead>
-          <TableBody>
+          <TableBody >
             {appointments.map((row) => (
-              <TableRow key={row.time}>
+              <TableRow key={row.time} >
                 <TableCell component="th" scope="row">
                   {row.advisor}
                 </TableCell>
