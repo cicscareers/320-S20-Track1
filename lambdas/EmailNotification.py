c48bd2508e8fcbbd8edf8a0e888c098da5e006ff
DavidCincotta <decincotta@gmail.com>
2020-04-01 16:03:11 -0400

    MIMEemail

--- lambdas/EmailNotification.py
+++ lambdas/EmailNotification.py
@@ -1,13 +1,92 @@
-@staticmethod
+import os
+import boto3
+from botocore.exceptions import ClientError
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+# SENDER should be imported from constants.py
+
+
+# Specify a configuration set. If you do not want to use a configuration
+# set, comment the following variable, and the 
+# ConfigurationSetName=CONFIGURATION_SET argument below.
+CONFIGURATION_SET = "ConfigSet"
+SENDER = 'umassreachout.dev@gmail.com'
+AWS_REGION = "us-east-1"
+CHARSET = "UTF-8"
+client = boto3.client('ses',region_name=AWS_REGION)
+
+
+
+example_subject = """
+HELLO SES $NAME$
+"""
+example_body = """
+<html>
+<head></head>
+<body>
+<h1>Amazon SES Test (SDK for Python)</h1>
+<p>This $NAME$ was sent with
+    <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+    <a href='https://aws.amazon.com/sdk-for-python/'>
+    AWS SDK for Python (Boto)</a>.</p>
+</body>
+</html>
+"""     
+
+# params: attribute map, template id
+# returns: tuple(recipient, subject, body, attachments)
 def generate_email(attributes, template_id):
+    # define body variable according to template_id
+    # using example_body, for our purposes
+    body = example_body
+    subject = example_subject
 
-    
-    return('TO', 'FROM', 'SUBJECT', 'BODY', 'ATTACHMENTS')
+    # attributes is a hashmap
+    for attribute in attributes.keys():
+        body.replace('$'+str(attribute)+'$', attributes[attribute])
+        subject.replace('$'+str(attribute)+'$', attributes[attribute])
 
+    msg = MIMEMultipart('mixed')
+    # Add subject, from and to lines.
+    msg['Subject'] = subject 
+    msg['From'] = SENDER 
+    msg['To'] = str(attributes['RECIPIENT'])
 
-@staticmethod
-def send_email(to, fromE, subject, body, attachments):
+    # Create a multipart/alternative child container.
+    msg_body = MIMEMultipart('alternative')
+    htmlpart = MIMEText(body.encode(CHARSET), 'html', CHARSET)
+    msg_body.attach(htmlpart)
+    msg.attach(msg_body)
+    if(attributes['attachments'] != None):
+        att = MIMEApplication(open(attributes('attachments'), 'rb').read())
+        att.add_header('Content-Disposition','attachment',filename=os.path.basename(attributes['attachments']))
+    msg.attach(att)
+    return tuple(str(SENDER), str(attributes['RECIPIENT']), str(msg.as_string()))
 
-    return True
+# params: tuple(recipient, subject, body, attachments)
+# returns: boolean
+def send_email(contentTuple):
+# Try to send the email.
+    try:
+        #Provide the contents of the email.
+        response = client.send_raw_email(
+            Source=contentTuple[0],
+            Destinations=[
+                contentTuple[1]
+            ],
+            RawMessage={
+                'Data':contentTuple[2],
+            },
+            ConfigurationSetName=CONFIGURATION_SET
+        )
+    # Display an error if something goes wrong.	
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
 
-
+# Testing function calls
+attributeTESTmap = {'name':'David Cincotta', 'subject':'SENDING'}
+send_email(generate_email(attributeTESTmap, 999))
--- lambdas/SES_exampleCode.py
+++ lambdas/SES_exampleCode.py
@@ -78,3 +78,107 @@ except ClientError as e:
 else:
     print("Email sent! Message ID:"),
     print(response['MessageId'])
+
+
+
+
+
+
+import os
+import boto3
+from botocore.exceptions import ClientError
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
+# Replace sender@example.com with your "From" address.
+# This address must be verified with Amazon SES.
+SENDER = "Sender Name <sender@example.com>"
+
+# Replace recipient@example.com with a "To" address. If your account 
+# is still in the sandbox, this address must be verified.
+RECIPIENT = "recipient@example.com"
+
+# Specify a configuration set. If you do not want to use a configuration
+# set, comment the following variable, and the 
+# ConfigurationSetName=CONFIGURATION_SET argument below.
+CONFIGURATION_SET = "ConfigSet"
+
+# If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+AWS_REGION = "us-west-2"
+
+# The subject line for the email.
+SUBJECT = "Customer service contact info"
+
+# The full path to the file that will be attached to the email.
+ATTACHMENT = "path/to/customers-to-contact.xlsx"
+
+# The email body for recipients with non-HTML email clients.
+BODY_TEXT = "Hello,\r\nPlease see the attached file for a list of customers to contact."
+
+# The HTML body of the email.
+BODY_HTML = """\
+<html>
+<head></head>
+<body>
+<h1>Hello!</h1>
+<p>Please see the attached file for a list of customers to contact.</p>
+</body>
+</html>
+"""
+
+# The character encoding for the email.
+CHARSET = "utf-8"
+
+# Create a new SES resource and specify a region.
+client = boto3.client('ses',region_name=AWS_REGION)
+
+# Create a multipart/mixed parent container.
+msg = MIMEMultipart('mixed')
+# Add subject, from and to lines.
+msg['Subject'] = SUBJECT 
+msg['From'] = SENDER 
+msg['To'] = RECIPIENT
+
+# Create a multipart/alternative child container.
+msg_body = MIMEMultipart('alternative')
+
+# Encode the text and HTML content and set the character encoding. This step is
+# necessary if you're sending a message with characters outside the ASCII range.
+htmlpart = MIMEText(BODY_HTML.encode(CHARSET), 'html', CHARSET)
+
+# Add the text and HTML parts to the child container.
+msg_body.attach(htmlpart)
+
+# Define the attachment part and encode it using MIMEApplication.
+att = MIMEApplication(open(ATTACHMENT, 'rb').read())
+
+# Add a header to tell the email client to treat this part as an attachment,
+# and to give the attachment a name.
+att.add_header('Content-Disposition','attachment',filename=os.path.basename(ATTACHMENT))
+
+# Attach the multipart/alternative child container to the multipart/mixed
+# parent container.
+msg.attach(msg_body)
+
+# Add the attachment to the parent container.
+msg.attach(att)
+#print(msg)
+try:
+    #Provide the contents of the email.
+    response = client.send_raw_email(
+        Source=SENDER,
+        Destinations=[
+            RECIPIENT
+        ],
+        RawMessage={
+            'Data':msg.as_string(),
+        },
+        ConfigurationSetName=CONFIGURATION_SET
+    )
+# Display an error if something goes wrong.	
+except ClientError as e:
+    print(e.response['Error']['Message'])
+else:
+    print("Email sent! Message ID:"),
+    print(response['MessageId'])
