d5c3d87a827c6b837c20e7a5b3fcc735518a8753
Matt Hill <mattjhill@umass.edu>
2020-04-02 13:24:43 -0400

    commit changing error

--- get_appointment_students.py
+++ get_appointment_students.py
@@ -4,21 +4,23 @@ import constants
 
 #Input: supporter_id
 #Output: JSON object of current supporter appointments
-def get_appointment_supporters(event, context):
+def get_appointment_students(event, context):
     # TODO implement
 
-    supporter_id_want = event['supporter_id']
+    student_id_want = event['student_id']
     client = boto3.client('rds-data') #Connecting to the database
     appointment_info = client.execute_statement(
         secretArn = constants.SECRET_ARN,
         database = constants.DB_NAME,
         resourceArn = constants.Arn,
-        sql = 'SELECT U.first_name, U.last_name, APP.supporter_id, APP.appointment_id, APP.time, APP.type, APP.duration, APP.method, APP.location FROM \
-            users U, supporters S, scheduled_appointments APP WHERE U.id = S.user_id AND S.supporter_id = APP.supporter_id AND APP.supporter_id = %s;' % (supporter_id_want)
+        sql = 'SELECT U.first_name, U.last_name, S.student_id, APP.appointment_id, APP.time, APP.type, APP.duration, APP.method, APP.location FROM users U, students S, student_appointment_relation R, \
+            scheduled_appointments APP WHERE U.id = S.user_id AND S.student_id = R.student_id AND R.appointment_id = APP.appointment_id AND S.student_id = %s;' % (student_id_want)
+            
+
     )
 
     """
-    if appointment_info['supporter_id'] == '': 
+    if appointment_info['student_id'] == '': 
         print("The user does not exist")
         return {
             "statusCode": 404
