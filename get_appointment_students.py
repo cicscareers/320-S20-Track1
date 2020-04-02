ce4f3a46880ea597baf6d927bdfa8afe227ec9c2
Matt Hill <mattjhill@umass.edu>
2020-04-02 13:35:54 -0400

    commit commenting constants

--- get_appointment_students.py
+++ get_appointment_students.py
@@ -1,6 +1,6 @@
 import json
 import boto3
-import constants
+#import constants
 
 #Input: supporter_id
 #Output: JSON object of current supporter appointments
@@ -33,7 +33,25 @@ def get_appointment_students(event, context):
     """
 
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from get_appointment_supporters Lambda!')
+   return{
+        "statusCode": 200,
+        "body": {
+            [
+                {
+                "advisor":"Ada Lovelace",
+                "subject": "Mock Interview",
+                "date": "10/10/2020",
+                "time": "10:00 - 10:30",
+                "actions": "Cancel"
+                },
+                {
+                "advisor":"Grace Hopper",
+                "subject": "Resume",
+                "date": "10/10/2020",
+                "time": "12:00 - 1:00",
+                "actions": "Cancel"
+                }
+            ]
+
+        }
     }
