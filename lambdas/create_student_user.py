57446251be5cedbae0a71d7f523d9e923696b576
Matt Hill <mattjhill@umass.edu>
2020-04-09 14:22:12 -0400

    added a insert into students statement

--- lambdas/create_student_user.py
+++ lambdas/create_student_user.py
@@ -7,8 +7,8 @@ import boto3
 #Output: 
 def create_student_user(event, context):
     
-    email = event['student_id']
-    password = event['hashed_password']
+    email = "test2email" #event['student_id']
+    password = "test2pass" #event['hashed_password']
     client = boto3.client('rds-data') #Connecting to the database
 
     #Need to check if email is already in DB
@@ -24,7 +24,8 @@ def create_student_user(event, context):
         return{
             'statusCode': 404 
         }
-    create = client.execute_statement(
+    # adds a user to the "users" table
+    create_users_instance = client.execute_statement(
         secretArn = "arn:aws:secretsmanager:us-east-2:500514381816:secret:rds-db-credentials/cluster-33FXTTBJUA6VTIJBXQWHEGXQRE/postgres-3QyWu7",
         database = "postgres",
         resourceArn = "arn:aws:rds:us-east-2:500514381816:cluster:postgres",
@@ -32,12 +33,20 @@ def create_student_user(event, context):
         VALUES ((SELECT id FROM users ORDER BY id DESC LIMIT 1)+1,'fn','ln','%s','pn','pic','bio','pro','gen','pho',false,true,'%s','salt')" % (email,password)
     )
 
-    if(create['numberOfRecordsUpdated'] == 0): 
+    if(create_users_instance['numberOfRecordsUpdated'] == 0): 
         print("Student user not created")
         return {
             "statusCode": 404
         }
     else:
+        # adds a user to the "students" table with the same user id
+        create_users_instance = client.execute_statement(
+        secretArn = "arn:aws:secretsmanager:us-east-2:500514381816:secret:rds-db-credentials/cluster-33FXTTBJUA6VTIJBXQWHEGXQRE/postgres-3QyWu7",
+        database = "postgres",
+        resourceArn = "arn:aws:rds:us-east-2:500514381816:cluster:postgres",
+        sql = "INSERT INTO students(student_id,user_id,spire_id,college,grad_year,resume,job_search,grad_student) \
+        VALUES ((SELECT id FROM users ORDER BY id DESC LIMIT 1),(SELECT id FROM users ORDER BY id DESC LIMIT 1),1000,'college',3000,'resume',false,false)"
+    )
         print("Student user created")
         return{
             'statusCode': 201
