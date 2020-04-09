99f9614579ea58b844dfaba6661c787d517cbf7c
dduong0ff <dduong@umass.edu>
2020-04-09 15:30:41 -0400

    Update create_supporter.py
    check if user exists and checked if loaded into db

--- lambdas/create_supporter.py
+++ lambdas/create_supporter.py
@@ -14,10 +14,43 @@ def create_supporter(event, context):
     print('OOF')
 
     email = event['email']
-    password = event['password']
+    password = event['hashed_password']
 
     client = boto3.client('rds-data')  # Connecting to Database
 
+    # checking if user exists
+    check_user = client.execute_statement(
+        secretArn="arn:aws:secretsmanager:us-east-2:500514381816:secret:rds-db-credentials/cluster-33FXTTBJUA6VTIJBXQWHEGXQRE/postgres-3QyWu7",
+        database="postgres",
+        resourceArn="arn:aws:rds:us-east-2:500514381816:cluster:postgres",
+        sql="SELECT email from users where email = '%s';" % email
+    )
+
+    if check_user['records'] != []:
+        print("This email exists already")
+        return {
+            'statusCode': 404
+        }
+
+    new_user = client.execute_statement(
+        secretArn="arn:aws:secretsmanager:us-east-2:500514381816:secret:rds-db-credentials/cluster-33FXTTBJUA6VTIJBXQWHEGXQRE/postgres-3QyWu7",
+        database="postgres",
+        resourceArn="arn:aws:rds:us-east-2:500514381816:cluster:postgres",
+        sql="INSERT INTO users(email, hashed_password) VALUES ('%s', '%s')" % (
+            email, password)
+
+    )
+
+    # check if Supporter data successfully loaded
+    if new_user['numberOfRecordsUpdated'] == 0:
+        print("Supporter not created")
+        return {
+            'statusCode': 404
+        }
+
+    # need request to admin about qualifying
+
+    print("Supporter created")
     return {
         'statusCode': 201
     }
