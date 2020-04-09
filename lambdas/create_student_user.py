2c4a3f7791dd25f3506533e64f604ee3e1c8e9f5
Matt Hill <mattjhill@umass.edu>
2020-04-09 12:27:58 -0400

    added a check duplicate email feature

--- lambdas/create_student_user.py
+++ lambdas/create_student_user.py
@@ -1,25 +1,38 @@
 import json
 import boto3
 
+#written by Matt Hill
+#4/8/20
 #Input: email, password
 #Output: 
 def create_student_user(event, context):
     
-    email = event['student_id']
-    password = event['password']
+    email = "testEmail"     #event['student_id']
+    password = "testPassword"           #event['password']
     client = boto3.client('rds-data') #Connecting to the database
+
+    #Need to check if email is already in DB
+    check_email = client.execute_statement(
+        secretArn = "arn:aws:secretsmanager:us-east-2:500514381816:secret:rds-db-credentials/cluster-33FXTTBJUA6VTIJBXQWHEGXQRE/postgres-3QyWu7",
+        database = "postgres",
+        resourceArn = "arn:aws:rds:us-east-2:500514381816:cluster:postgres",
+        sql = "SELECT email FROM users WHERE email = '%s';" % (email)
+    )
+
+    if(check_email['records'] != []):
+        print("email is already being used")
+        return{
+            'statusCode': 404 
+        }
     create = client.execute_statement(
         secretArn = "arn:aws:secretsmanager:us-east-2:500514381816:secret:rds-db-credentials/cluster-33FXTTBJUA6VTIJBXQWHEGXQRE/postgres-3QyWu7",
         database = "postgres",
         resourceArn = "arn:aws:rds:us-east-2:500514381816:cluster:postgres",
-        sql = "INSERT INTO students VALUES ('%s','%s');" % (email,password)
+        sql = "INSERT INTO users(id,first_name,last_name, email, preferred_name, picture, bio,pronouns,gender,phone,is_blocked,GCal_permission,hashed_password,salt_key) \
+        VALUES ((SELECT id FROM users ORDER BY id DESC LIMIT 1)+1,'fn','ln','%s','pn','pic','bio','pro','gen','pho',false,true,'%s','salt')" % (email,password)
     )
 
-<<<<<<< Updated upstream:lambdas/create_student_user.py
-    if create['records'] == '': 
-=======
     if(create['numberOfRecordsUpdated'] == 0): 
->>>>>>> Stashed changes:create_student_user.py
         print("Student user not created")
         return {
             "statusCode": 404
