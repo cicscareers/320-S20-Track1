0e3f5d69d89d9c1539ee60c9af582f830453aad6
dduong0ff <dduong@umass.edu>
2020-04-09 00:35:14 -0400

    Update create_supporter.py
    Outlined properties and provided inputs

--- lambdas/create_supporter.py
+++ lambdas/create_supporter.py
@@ -1,4 +1,23 @@
+import json
+import boto3
+
+
+# inputs:
+# Email ID, Password
+
+# then request from admin
+# restrict access to platform
+# once verified recieve email to login
 
 def create_supporter(event, context):
 
     print('OOF')
+
+    email = event['email']
+    password = event['password']
+
+    client = boto3.client('rds-data')  # Connecting to Database
+
+    return {
+        'statusCode': 201
+    }
