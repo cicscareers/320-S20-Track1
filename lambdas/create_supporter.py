47558dfa92f23b1c8ab417789bb4656ad213f8e3
dduong0ff <dduong@umass.edu>
2020-04-09 17:23:18 -0400

    Update create_supporter.py
    modified inserting users table
    added inserting supporters table

--- lambdas/create_supporter.py
+++ lambdas/create_supporter.py
@@ -5,9 +5,6 @@ import boto3
 # inputs:
 # Email ID, Password
 
-# then request from admin
-# restrict access to platform
-# once verified recieve email to login
 
 def create_supporter(event, context):
 
@@ -32,25 +29,40 @@ def create_supporter(event, context):
             'statusCode': 404
         }
 
+    # insert new user into users table
     new_user = client.execute_statement(
         secretArn="arn:aws:secretsmanager:us-east-2:500514381816:secret:rds-db-credentials/cluster-33FXTTBJUA6VTIJBXQWHEGXQRE/postgres-3QyWu7",
         database="postgres",
         resourceArn="arn:aws:rds:us-east-2:500514381816:cluster:postgres",
-        sql="INSERT INTO users(email, hashed_password) VALUES ('%s', '%s')" % (
-            email, password)
-
+        sql="INSERT INTO users(id,first_name,last_name, email, preferred_name, picture, bio, pronouns, gender, phone, is_blocked,GCal_permission, hashed_password, salt_key) \
+        VALUES ((SELECT id FROM users ORDER BY id DESC LIMIT 1)+1,'fn','ln','%s','pn','pic','bio','pro','gen','pho',false,true,'%s','salt')" % (email, password)
     )
 
-    # check if Supporter data successfully loaded
+    # check if user data successfully loaded
     if new_user['numberOfRecordsUpdated'] == 0:
-        print("Supporter not created")
+        print("User was not created")
         return {
             'statusCode': 404
         }
 
-    # need request to admin about qualifying
+    # inserts user into supporters table with same user_id
+    else:
+        new_user = client.execute_statement(
+            secretArn="arn:aws:secretsmanager:us-east-2:500514381816:secret:rds-db-credentials/cluster-33FXTTBJUA6VTIJBXQWHEGXQRE/postgres-3QyWu7",
+            database="postgres",
+            resourceArn="arn:aws:rds:us-east-2:500514381816:cluster:postgres",
+            sql="INSERT INTO supporters(supporter_id, user_id, employer, title, team, feedback, rating, team_name) \
+                VALUES ((SELECT id FROM users ORDER BY id DESC LIMIT 1),(SELECT id FROM users ORDER BY id DESC LIMIT 1), 'employer', 'title', 'team', false, 0, 'team name')"
+        )
 
-    print("Supporter created")
-    return {
-        'statusCode': 201
-    }
+        # check if supporter data successfully loaded
+        if new_user['numberOfRecordsUpdated'] == 0:
+            print("Supporter was not created")
+            return {
+                'statusCode': 404
+            }
+
+        print("Supporter created")
+        return {
+            'statusCode': 201
+        }
