3abd783a9c6478b6563b182084bb77eebd76c6e1
Hadley Pope <hpope@umass.edu>
2020-04-10 12:52:00 -0400

    WIP login git example

--- lambdas/login.py
+++ lambdas/login.py
@@ -1,15 +1,9 @@
-import bcrypt
 import query from query_db
+import datetime
 
-def hash_password(email, password):
-    sql = "SELECT salt_key FROM users WHERE email = '%s';" % (email)
-    salt_key = query(sql)
-    return bcrypt.hashpw(password, salt_key)
-
-
-def verify_password(email, given_password, stored_password):
-    hashed_given_password = hash_password(email, given_password)
-    return hashed_given_password == stored_password
+JWT_SECRET = 'secret'
+JWT_ALGORITHM = 'HS265'
+JWT_EXP_DELTA_SECONDS = 86400
 
 def login(http_body):
     
@@ -34,34 +28,46 @@ def login(http_body):
         # constants.STATUS_CODE = 404
         return
 
+    print("User exists! Fetching name...") 
+    sql = "SELECT first_name FROM users WHERE email = '%s';" % (given_email)
+    f_name = query(sql)
+
+    sql = "SELECT last_name FROM users WHERE email = '%s';" % (given_email)
+    l_name = query(sql)
+
     #Get password from existing user and if does not match return a 400 http
-    print("User exists! Acquiring password...")
+    print("Acquiring password...")
     sql = "SELECT hashed_password FROM users WHERE email = '%s';" % (given_email)
-    existing_password = query(sql)
+    existing_password = query(sql)['records'][0][0]['stringValue']
 
     print("Checking password...")
-    if not verify_password(given_email, given_password, existing_password['records'][0][0]['stringValue']):
+    if not given_password == existing_password:
         # constants.ERR = "Password DNE"
         # constants.STATUS_CODE = 404
         return
 
     #Get user type from Database
-    print("Password verified. Checking perms...")
-    sql = "SELECT type FROM users WHERE email = '%s';" % (given_email)
-    user_type = query(sql)
+    print("Password verified. Checking role...")
+    sql = "SELECT user_id FROM users WHERE email = '%s';" % (given_email)
+    
+   
 
-    user_type = user_type['records'][0][0]['longValue']
-    if user_type == 1:
-        user_type = constants.STUDENT
-    elif user_type == 2:
-        user_type = constants.SUPPORTER
-    else:
-        user_type = constants.ADMIN
-    token = create_token.rand_token()
+    token_payload = {
+        'email' : given_email,
+        'role' : role,
+        'exp' : datetime.utcnow() + timedelta(seconds = JWT_EXP_DELTA_SECONDS)
+    }
+    token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
 
     print("Done!")
-    db_config.RES = {'token': str(token), 'user':str(user_type)}
-    return
+    response_body = {
+        'token': token, 
+        'email' : given_email,
+        'f_name':,
+        'l_name':,
+        'role':
+        }
+    return response_body
 
 def login_handler(event, context):
     return {
