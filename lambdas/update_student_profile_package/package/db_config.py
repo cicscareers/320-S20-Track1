52c10885f171566af93b68d1e17cd53cffe64bfd
vcaruso1 <vcaruso@umass.edu>
2020-04-11 01:49:28 -0400

    changed sql queries to work

---
+++ lambdas/update_student_profile_package/package/db_config.py
@@ -0,0 +1,5 @@
+
+DB_NAME = "postgres"
+ARN = "arn:aws:rds:us-east-2:500514381816:cluster:postgres"
+SECRET_ARN = "arn:aws:secretsmanager:us-east-2:500514381816:secret:rds-db-credentials/cluster-33FXTTBJUA6VTIJBXQWHEGXQRE/postgres-3QyWu7"
+
