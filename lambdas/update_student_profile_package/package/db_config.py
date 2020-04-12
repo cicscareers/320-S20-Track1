6819953639fdb928da961d8be4cc46becdd21ce3
Maeve Newman <maevenewman@gmail.com>
2020-04-11 23:05:57 -0400

    Revert "Merge branch 'BackendLambda' of https://github.com/david-fisher/320-S20-Track1 into BackendLambda"
    This reverts commit ffab64033b191e5116c919fec34d0c8bf4b06747, reversing
    changes made to f74551d175aab5e13aa877ae7a7d6553ea581dab.

--- lambdas/update_student_profile_package/package/db_config.py
+++
@@ -1,5 +0,0 @@
-
-DB_NAME = "postgres"
-ARN = "arn:aws:rds:us-east-2:500514381816:cluster:postgres"
-SECRET_ARN = "arn:aws:secretsmanager:us-east-2:500514381816:secret:rds-db-credentials/cluster-33FXTTBJUA6VTIJBXQWHEGXQRE/postgres-3QyWu7"
-
