e522f983529bce8a8db4e68e5156d888b33dd9a5
Matt Hill <mattjhill@umass.edu>
2020-04-14 15:36:13 -0400

    made return easier to read for front end

--- lambdas/get_appointment_students.py
+++ lambdas/get_appointment_students.py
@@ -3,7 +3,7 @@ import boto3
 
 #Input: supporter_id
 #Output: JSON object of current appointments that a student has in the format: 
-# "student first name", "student last name", "supporter first name", "supporter last name", "type", "duration","method","location"
+# "studentFN", "studentLN", "supporterFN", "supporterLN", "type", "duration","method","location"
 
 def get_appointment_students(event, context):
     
@@ -24,8 +24,23 @@ def get_appointment_students(event, context):
             'statusCode': 404
         }
     else:
+        student_appointments = []
+
+        #For each entry in the query data, extracts relevant data and stores it in a dictionary with appropriate key
+        for entry in appointment_info['records']: 
+            block = dict()
+            block["studentFN"] = entry[0].get("stringValue")
+            block["studentLN"] = entry[1].get("stringValue")
+            block["supporterFN"] = entry[2].get("stringValue")
+            block["supporterLN"] = entry[3].get("stringValue")
+            block["type"] = entry[4].get("stringValue")
+            block["duration"] = entry[5].get("longValue")
+            block["method"] = entry[6].get("stringValue")
+            block["location"] = entry[7].get("stringValue")
+            student_appointments.append(block)
+
         return{
-            'body': json.dumps(appointment_info['records']), #outputs the query in JSON format
+            'body': json.dumps(student_appointments), #outputs the query in JSON format
             'statusCode': 200
         }
     
