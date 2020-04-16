53deb6f63f7cbd42066e206c4365c2a9ec684df9
Hadley Pope <hpope@umass.edu>
2020-04-16 12:32:44 -0400

    Pulled in most recent version of query_db

--- lambdas/package/query_db.py
+++ lambdas/package/query_db.py
@@ -1,13 +1,20 @@
+from package.lambda_exception import LambdaException
+from package import db_config
 import boto3
-import db_config
 
 secretArn = db_config.SECRET_ARN
 dbName = db_config.DB_NAME
 arn = db_config.ARN
 
 def query(sql_query):
-    client = boto3.client("rds-data")
+    try:
+        client = boto3.client("rds-data")
+    except:
+        raise LambdaException("Cannot connect to Database")
 
-    result = client.execute_statement(secretArn, dbName, arn, sql_query)
+    try:
+        result = client.execute_statement(secretArn=secretArn, database=dbName, resourceArn=arn, sql=sql_query)
+    except:
+        raise LambdaException("Invalid query")
 
     return result
