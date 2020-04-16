8a731dc0d018a883b98c116deec3d18eba871d57
nhanfiction <nhan.le.7062@gmail.com>
2020-04-16 15:51:03 -0400

    Parameterized queries

--- lambdas/update_supporter_appointment_settings.py
+++ lambdas/update_supporter_appointment_settings.py
@@ -1,8 +1,7 @@
 #Written by Nhan Le
 
+from package.query_db import query
 import json
-import boto3
-import constants
 
 def update_supporter_appointment_settings(event, context):
     supporter_id = event['id']
@@ -13,42 +12,61 @@ def update_supporter_appointment_settings(event, context):
     job_search = event['job_search']
     grad_student = event['grad_student']
 
-    #Connect to table
-    client = boto3.client('rds-data')
+    preferences = {}
+    specializations = {}
 
-    #Check if supporter exists
-    existing_user = client.execute_statement(
-        secretArn = constants.SECRET_ARN, 
-        database = constants.DB_NAME,
-        resourceArn = constants.ARN,
-        sql = "SELECT supporter_id FROM supporters WHERE supporter_id = '%d';" % (supporter_id)
-    )
-    if(existing_user['records'] == []):
-        return{
-            'statuscode' : 404
-        }
-
-    query = ""
-
-    #Can't be null
+    #Adding to preferences dictionary
+    #The reason I add to the dictionary by adding another dictionary with the value type
+    #and then value is for the parameterization
+    if(job_search != None):
+        preferences['job_search'] = {'booleanValue': job_search}
+    if(grad_student != None):
+        preferences['grad_student'] = {'booleanValue': grad_student}
     if(job_search == None or grad_student == None): #These values cannot be null
         return{
             'statusCode' : 422 #unproccesable
         }
+    preferences['supporter_id'] = {'longValue': supporter_id}
 
-    preferences_table = "UPDATE supporter_preferences_for_students SET job_search = '%s', grad_student = '%s' WHERE supporter_id = '%d';" % (job_search, grad_student, supporter_id)
-    specialization_table = "UPDATE supporter_specializations SET max_students = '%d', duration = '%d', specialization_type_id = '%d' WHERE supporter_id = '%d';" % (max_students, duration, specialization_type_id, supporter_id)
-    major_preferences_table = "INSERT INTO supporter_major_preferences (supporter_id, major_id) VALUES ('%d', '%d');" % (supporter_id, major_id)
+    #Adding to specializations dictionary
+    specializations['max_students'] = {'longValue': max_students}
+    specializations['duration'] = {'longValue': duration}
+    specializations['specialization_type_id'] = {'longValue': specialization_type_id}
+    specializations['supporter_id'] = {'longValue': supporter_id}
 
-    query = preferences_table + " " + specialization_table + " " + major_preferences_table
 
-    client.execute_statement(
-        secretArn = constants.SECRET_ARN, 
-        database = constants.DB_NAME,
-        resourceArn = constants.ARN,
-        sql = query
-    )
+    #Execute parameterized query for updating preferences
+    sql = 'UPDATE supporter_preferences_for_students SET job_search = :job_search, grad_student = :grad_student WHERE supporter_id = :supporter_id;'
+    sql_parameters = dictionary_to_list(preferences)
+    response = query(sql, sql_parameters)
+    response['records']
+    #check response for status code
+
+    #Execute parameterized query for updating specializations
+    sql = 'UPDATE supporter_specializations SET max_students = :max_student, duration = :duration, \
+        specialization_type_id = :specialization_type_id WHERE supporter_id = :supporter_id;'
+    sql_parameters = dictionary_to_list(specializations)
+    response = query(sql, sql_parameters)
+    response['records']
+    #check response for status code
+
+    #Execute parameterized query for inserting major preference
+    sql = 'INSERT INTO supporter_major_preferences (supporter_id, major_id) values (:supporter_id, :major_id)'
+    sql_parameters = [
+        {'name': 'supporter_id', 'value': {'longValue': supporter_id}},
+        {'name': 'major_id', 'value': {'longValue': major_id}}
+    ]
+    response = query(sql, sql_parameters)
+    response["numberOfRecordsUpdated"]
+    #check response for status code
 
     return {
         'statusCode': 200
     }
+
+def dictionary_to_list(dictionary):
+    params = []
+    #For each key, name = key and value = dictionary {value type : value}
+    for key in dictionary.keys():
+        params.append({'name': key, 'value': dictionary[key]})
+    return params
