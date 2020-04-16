029d61d157cc7a1c61cec988ba0707eff453abf9
Matt Hill <mattjhill@umass.edu>
2020-04-16 19:16:10 -0400

    updated to paramaterized query

--- lambdas/create_student_user.py
+++ lambdas/create_student_user.py
@@ -1,5 +1,5 @@
 import json
-import boto3
+from package.query_db import query
 
 #written by Matt Hill
 #4/8/20
@@ -11,15 +11,11 @@ def create_student_user(event, context):
     last_name = event['last_name']
     email = event['email']
     password = event['hashed_password']
-    client = boto3.client('rds-data') #Connecting to the database
 
     #Need to check if email is already in DB
-    check_email = client.execute_statement(
-        secretArn = "arn:aws:secretsmanager:us-east-2:500514381816:secret:rds-db-credentials/cluster-33FXTTBJUA6VTIJBXQWHEGXQRE/postgres-3QyWu7",
-        database = "postgres",
-        resourceArn = "arn:aws:rds:us-east-2:500514381816:cluster:postgres",
-        sql = "SELECT email FROM users WHERE email = '%s';" % (email)
-    )
+    sql = "SELECT email FROM users WHERE email = :email"
+    sql_parameters = [{'name' : 'email', 'value': {'stringValue' : email}}]
+    check_email = query(sql, sql_parameters)
 
     if(check_email['records'] != []):
         return{
@@ -27,22 +23,27 @@ def create_student_user(event, context):
             'statusCode': 409
         }
     # generates a new id 
-    new_id_query = client.execute_statement(
-        secretArn = "arn:aws:secretsmanager:us-east-2:500514381816:secret:rds-db-credentials/cluster-33FXTTBJUA6VTIJBXQWHEGXQRE/postgres-3QyWu7",
-        database = "postgres",
-        resourceArn = "arn:aws:rds:us-east-2:500514381816:cluster:postgres",
-        sql = "SELECT id FROM users ORDER BY id DESC LIMIT 1;"
-    )
+    sql = "SELECT id FROM users ORDER BY id DESC LIMIT 1"
+    sql_parameters = []
+    new_id_query = query(sql,sql_parameters)
+#    new_id_query = client.execute_statement(
+#        secretArn = "arn:aws:secretsmanager:us-east-2:500514381816:secret:rds-db-credentials/cluster-33FXTTBJUA6VTIJBXQWHEGXQRE/postgres-3QyWu7",
+#        database = "postgres",
+#        resourceArn = "arn:aws:rds:us-east-2:500514381816:cluster:postgres",
+#        sql = "SELECT id FROM users ORDER BY id DESC LIMIT 1;"
+#    )
     new_id = new_id_query['records'][0][0]['longValue'] + 1
 
     # adds a user to the "users" table with the new generated id
-    create_users_instance = client.execute_statement(
-        secretArn = "arn:aws:secretsmanager:us-east-2:500514381816:secret:rds-db-credentials/cluster-33FXTTBJUA6VTIJBXQWHEGXQRE/postgres-3QyWu7",
-        database = "postgres",
-        resourceArn = "arn:aws:rds:us-east-2:500514381816:cluster:postgres",
-        sql = "INSERT INTO users(id,first_name,last_name, email, preferred_name, picture, bio,pronouns,gender,phone,is_blocked,GCal_permission,hashed_password,salt_key,user_type) \
-        VALUES ('%s','%s','%s','%s','pn','pic','bio','pro','gen','pho',false,true,'%s','salt','student')" % (new_id,first_name,last_name,email,password)
-    )
+    sql = "INSERT INTO users(id,first_name,last_name, email, preferred_name, picture, bio,pronouns,gender,phone,is_blocked,GCal_permission,hashed_password,salt_key,user_type) \
+        VALUES (':new_id',':first_name',':last_name',':email','pn','pic','bio','pro','gen','pho',false,true,':password','salt','student')"
+    sql_parameters = [{'name' : 'new_id', 'value': {'longValue' : new_id}},
+    {'name' : 'first_name', 'value': {'stringValue' : first_name}},
+    {'name' : 'last_name', 'value': {'stringValue' : last_name}},
+    {'name' : 'email', 'value': {'stringValue' : email}},
+    {'name' : 'password', 'value': {'stringValue' : password}}]
+
+    create_users_instance = query(sql,sql_parameters)
 
     if(create_users_instance['numberOfRecordsUpdated'] == 0): 
         return {
@@ -51,13 +52,11 @@ def create_student_user(event, context):
         }
     else:
         # adds a user to the "students" table with the same user id
-        create_users_instance = client.execute_statement(
-        secretArn = "arn:aws:secretsmanager:us-east-2:500514381816:secret:rds-db-credentials/cluster-33FXTTBJUA6VTIJBXQWHEGXQRE/postgres-3QyWu7",
-        database = "postgres",
-        resourceArn = "arn:aws:rds:us-east-2:500514381816:cluster:postgres",
         sql = "INSERT INTO students(student_id,user_id,spire_id,college,grad_year,resume,job_search,grad_student) \
-        VALUES ('%s','%s',1000,'college',4000,'resume',false,false)" % (new_id, new_id)
-    )
+        VALUES (':new_id',':new_id',1000,'college',4000,'resume',false,false)"
+        sql_parameters = [{'name' : 'new_id', 'value': {'longValue' : new_id}},
+                        {'name' : 'new_id', 'value': {'longValue' : new_id}}]
+        create_users_instance = query(sql,sql_parameters)
         
         return{
             'body': json.dumps("Student user created"),
