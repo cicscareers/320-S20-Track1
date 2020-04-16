7b15485c0d6973cc5b2d146611c7dc23e136e08b
Hadley Pope <hpope@umass.edu>
2020-04-16 12:35:40 -0400

    Added parameterized querying

--- lambdas/package/query_db.py
+++ lambdas/package/query_db.py
@@ -6,14 +6,14 @@ secretArn = db_config.SECRET_ARN
 dbName = db_config.DB_NAME
 arn = db_config.ARN
 
-def query(sql_query):
+def query(sql_query, sql_parameters=[]):
     try:
         client = boto3.client("rds-data")
     except:
         raise LambdaException("Cannot connect to Database")
 
     try:
-        result = client.execute_statement(secretArn=secretArn, database=dbName, resourceArn=arn, sql=sql_query)
+        result = client.execute_statement(secretArn=secretArn, database=dbName, resourceArn=arn, sql=sql_query, parameters=sql_parameters)
     except:
         raise LambdaException("Invalid query")
 
