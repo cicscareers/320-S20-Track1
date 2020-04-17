f5ede26a59fbaa40f96ac956c4ea08fdc38689e7
Kyle O'Leary <kyleoleary10@gmail.com>
2020-04-17 13:20:34 -0400

    Added 2 panels to admin settings

--- Client/src/Pages/adminSettings.js
+++ Client/src/Pages/adminSettings.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import Link from "@material-ui/core/Link";
@@ -6,7 +6,28 @@ import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
-export default function adminSettings() {
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Button from "@material-ui/core/Button";
+import Topics from "../components/topics.js"
+
+
+
+export default function AdminSettings() {
+  const fieldDefaults = [
+    'A',
+    'B',
+    'C',
+    'D'
+  ]
+  const [curField, setCurrField] = useState("");
+  const [newDefault, setNewDefault] = useState("");
+  const [selectedTopic, setSelectedTopic] = useState("");
+  const [addTopic, setAddTopic] = useState("");
+
+
   return (
     // <Container component="main" maxWidth="xs" align="center">
     //   <Typography component="h1" variant="h5" align="center">
@@ -21,58 +42,106 @@ export default function adminSettings() {
               Admin Settings
           </Typography>
         </Grid>
-
-      <Grid containtr Item style={{marginLeft:'25px'}}>
-        <Grid Item Container>
-          <Typography component="h1" variant="h5">
-              Field Defaults
-          </Typography>
-            <Grid Item container direction="column">
-              <Grid Item container style={{marginLeft:'25px'}}>
-                <Grid Item>
-                    <Typography component="h1" variant="h6">
-                      Test:
-                    </Typography>
-                </Grid>
-                <Grid Item>
-                  <Autocomplete
-                   id="value"
-                   options={1,2,3,4}//this isnt working
-                   style={{ width: 200 }}
-                   renderInput={(params) => <TextField label="Value" variant="outlined" id = "value"/> }
-                   />
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        <Grid Item>
-            <Typography component="h1" variant="h5">
-                Supporter Labels
-            </Typography>
-          </Grid>
-          <Grid Item>
-            <Typography component="h1" variant="h5">
-                Help Needed Topics
-            </Typography>
-          </Grid>
-          <Grid Item>
-            <Typography component="h1" variant="h5">
-                Appointment Types
-            </Typography>
-          </Grid>
-          <Grid Item>
-              <Typography component="h1" variant="h5">
-                  Block/Unblock User
+        <Grid item xs={12}>
+          <ExpansionPanel>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography>
+                Field Defaults
               </Typography>
-            </Grid>
-            <Grid Item>
-                <Typography component="h1" variant="h5">
-                    Download Data
-                </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Grid container spacing={1}>
+                <Grid item xs={3}>
+                  <Autocomplete
+                    id="field-defaults"
+                    options= {fieldDefaults}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        variant="outlined"
+                        label="Select a Field"
+                      />
+                    )}
+                    onChange={(e,v) => setCurrField(v)}
+                  />
+                </Grid>
+                <Grid item xs={2}>
+                  <TextField
+                    variant="outlined"
+                    id="default"
+                    fullWidth
+                    label="Set new:"
+                    name="field default"
+                    onChange={e => setNewDefault(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={1}>
+                  <Button variant='contained' color='primary' size='large'>
+                    Change
+                  </Button>
+                </Grid>
               </Grid>
-            </Grid>
-              </Paper>
-      </Grid>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography>
+                Supporter Topics
+              </Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Grid container spacing={1}>
+                <Grid item xs={3}>
+                  <Autocomplete
+                    id="supporter-topics"
+                    options= {Topics}
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        variant="outlined"
+                        label="Topics"
+                      />
+                    )}
+                    onChange={(e,v) => setSelectedTopic(v)}
+                  />
+                </Grid>
+                <Grid item xs={3}>
+                  <Button variant='contained' color='primary' size='large'>
+                    Delete Selected Topic
+                  </Button>
+                </Grid>
+                <Grid item xs={3}>
+                  <TextField
+                    variant="outlined"
+                    id="add-topic"
+                    fullWidth
+                    label="Topic to add:"
+                    name="add-topic"
+                    onChange={e => setAddTopic(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={3}>
+                  <Button variant='contained' color='primary' size='large'>
+                    Add Topic
+                  </Button>
+                </Grid>
+              </Grid>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>            
+        </Grid>
+
+
+      
+      </Paper>
+    </Grid>
 
   );
 }
