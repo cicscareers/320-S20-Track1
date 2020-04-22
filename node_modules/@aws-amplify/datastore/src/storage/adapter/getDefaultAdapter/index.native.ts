927a5c2814e6bd4b138d2dd1efabcf1a970dddf9
Sean Powell <powellsl99@gmail.com>
2020-04-22 14:01:26 -0400

    remove nodemodules

--- node_modules/@aws-amplify/datastore/src/storage/adapter/getDefaultAdapter/index.native.ts
+++
@@ -1,8 +0,0 @@
-import { Adapter } from '..';
-import AsyncStorageAdapter from '../asyncstorage';
-
-const getDefaultAdapter: () => Adapter = () => {
-	return AsyncStorageAdapter;
-};
-
-export default getDefaultAdapter;
