3467df25062da3bbc6d58a0fe7c640cde488bd66
Sean Powell <powellsl99@gmail.com>
2020-04-22 14:39:02 -0400

    fix routing issue

--- Client/src/Pages/home.js
+++ Client/src/Pages/home.js
@@ -1,76 +1,17 @@
-import React from 'react';
-import Calendar from 'react-calendar';
-import { Grid, Typography, Button, makeStyles, Box} from '@material-ui/core';
-import 'react-calendar/dist/Calendar.css';
-import Container from "@material-ui/core/Container";
-import Appointments from './appts';
+import React from "react";
+import Matching from "../Pages/Matching.js";
+import appts from "../Pages/appts.js";
 import Cookies from "universal-cookie";
+import { Route, Switch, Redirect } from "react-router-dom";
 
-const useStyles = makeStyles(theme => ({
-  paper: {
-    marginTop: theme.spacing(4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
-  },
-  avatar: {
-    
-    
-    //border: 50,
-    //borderBlockColor: '#000000'
-  },
-  selection: {
-    //width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-    marginLeft: theme.spacing(10)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2)
-  },
-  appointments: {
-    
+const cookies = new Cookies();
+const role = cookies.get("role");
+const redirect= () => {
+  if(role==="Student"){
+    <Redirect exact from="/" to="/match" />
+  }else{
+    <Redirect exact from="/" to="/appointments" />
   }
-}));
-
-function App() {
-  const cookies = new Cookies();
-  const fname = cookies.get("firstName");
-  const lname = cookies.get("lastName");
-  const role=cookies.get("role");
-  const classes = useStyles();
-  return (
-  	<Container component='main'>
-      <Typography component="h1" variant="h4" align="center" className={classes.paper}>
-        Welcome to ReachOUT {fname} {lname}!
-      </Typography>
-      <Grid container  lg={12} className={classes.selection}>
-      
-      </Grid>
-      
-      <Grid container lg={12} className={classes.selection}>
-        <Grid container lg={4}>
-          
-          <Calendar className = {classes.selection} />
-        </Grid>
-        <Grid container lg={2}></Grid>
-        <Grid contains lg={6} justify='center'>
-          
-          {Appointments()}
-           
-          
-        </Grid>
-          <Grid container lg={10} justify='center'>
-            <Button variant='contained'>Community News</Button>
-          </Grid>
-        
-      </Grid>
-    </Container>
-      
-     
-    
-    
-    
-  );
 }
 
-export default App;
+export default redirect
