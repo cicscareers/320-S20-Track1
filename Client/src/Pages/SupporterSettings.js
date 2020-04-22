f4d0a8ae7f48d97e321e09a72669b8ace0db0601
maddieviola <60367978+maddieviola@users.noreply.github.com>
2020-04-22 15:12:02 -0400

    Changed account.js
    WOrked on changing different fields

--- Client/src/Pages/SupporterSettings.js
+++ Client/src/Pages/SupporterSettings.js
@@ -3,6 +3,18 @@ import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from "@material-ui/core/TextField";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Button from "@material-ui/core/Button";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Topics from "../components/topics.js";
+import Tags from "../components/tags.js";
 
 export default function supporterSettings() {
   return (
