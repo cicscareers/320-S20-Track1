63479bd649f1617343be8247a842afe96f3d5147
Hadley Pope <hpope@umass.edu>
2020-04-21 20:45:52 -0400

    Fixed small errors

--- lambdas/get_user_roles.py
+++ lambdas/get_user_roles.py
@@ -4,12 +4,12 @@ from package.lambda_exception import LambdaException
 def get_user_roles_handler(event, context):
 
     user_id = int(event[user_id])
-    user_id_param = {'name' : 'user_id', 'value' : {'longValue' : user_id}}
+    user_id_param = [{'name' : 'user_id', 'value' : {'longValue' : user_id}}]
 
-    sql = "SELECT is_admin, is_supporter, is_user FROM users WHERE id = :user_id"
+    sql = "SELECT is_admin, is_supporter, is_student FROM users WHERE id = :user_id"
 
     try:
-        result = query(sql, user_id_param)['records']
+        result = query(sql, user_id_param)['records'][0]
     except Exception as e:
         raise LambdaException("Unable to retrieve roles: " + str(e))
 
@@ -23,5 +23,5 @@ def get_user_roles_handler(event, context):
 
     return {
         'statusCode' : 200,
-        'body' : user_roles
+        'user_roles' : user_roles
     }
