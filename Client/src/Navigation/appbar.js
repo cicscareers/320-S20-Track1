d17f137d9de11935a7a139eb90da102f7c50668c
V.M.Narpavirajan <nmuralidhara@umass.edu>
2020-04-22 20:19:03 -0400

    add switch users modal

--- Client/src/Navigation/appbar.js
+++ Client/src/Navigation/appbar.js
@@ -4,6 +4,18 @@ import {AppBar, Avatar, Toolbar, Typography, MenuItem, Button, Menu, Link,Dialog
 import Cookies from "universal-cookie";
 import Roles from'./role.json';
 import Chip from '@material-ui/core/Chip';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+// import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
@@ -104,7 +116,8 @@ export default function MenuAppBar(props) {
                 console.log("No Supporters Found");
             });
     }, [])
-
+   
+    
   const handleModalOpen = () => {
     setOpen(true);
   };
@@ -120,7 +133,7 @@ export default function MenuAppBar(props) {
   const handleClose = () => {
     setAnchorEl(null);
   };
-
+  
   //Function that handles log out by deleting the cookie and reloading
   function logout() {
     cookies.remove("email");
@@ -133,7 +146,7 @@ export default function MenuAppBar(props) {
  function renderNavBarButtonsBasedOnRole(){
    let RenderButtons=[];
    if(role.toLowerCase()==PossibleRoles[0]){
-     return(<div style={{width:'40%',float:'right'}}><Button variant="text" href="/" className={classes.button}>Find A Supporter</Button>
+     return(<div style={{width:'40%',float:'right'}}><Button variant="text" href="/match" className={classes.button}>Find A Supporter</Button>
      <Button variant="text" href="/appointments" className={classes.button}>Appointments</Button>
      <Button variant="text" href="/FAQ" className={classes.button}>FAQ</Button></div>);
 
@@ -146,33 +159,52 @@ export default function MenuAppBar(props) {
   
 
  }
+//  alert(PossibleRoles);
+//   const RoleNameStudent=PossibleRoles[0].charAt(0).toUpperCase() + PossibleRoles[0].slice(1);
+//   const RoleNameSupporter=PossibleRoles[1].charAt(0).toUpperCase() + PossibleRoles[1].slice(1);
+//   const RoleNameAdmin=PossibleRoles[2].charAt(0).toUpperCase() + PossibleRoles[2].slice(1);
  const SwitchUserHandle= event =>{
-  
+  if(event.currentTarget.id=='student'){
+    cookies.set('role',"Student");
+  }
+  else{
   cookies.set('role',event.currentTarget.id);
- 
-  window.location.reload();
+  }
+  if(cookies.get('role')=='Student'){
+    window.location.reload('/');
+  }
+ window.location.reload('/appointments');
+  
  }
  function renderRolesInModal(){
   let RenderRoles=[];
   var RolesList=Roles[0];
-  if(RolesList.roles.length==1){
+  
+  if(PossibleRoles.length==1){
     RenderRoles=(<h2>Sorry, You just have 1 role.</h2>);
   }
   else{
-
-    for(var i=0;i<RolesList.roles.length;i++){
-      if(RolesList.roles[i]==role){
+    
+    for(var i=0;i<PossibleRoles.length;i++){
+      var RoleName='';
+      RoleName=PossibleRoles[i].charAt(0).toUpperCase() + PossibleRoles[i].slice(1);
+     
+      if(PossibleRoles[i]==role.toLowerCase()){
         continue;
       }
-      RenderRoles.push((<Chip
-      id={RolesList.roles[i]}
+    
+      
+      RenderRoles.push((<Button classname={classes.button}
+      id={PossibleRoles[i]}
+      
        
-        label={RolesList.roles[i]}
-        clickable
         color="primary"
         onClick={SwitchUserHandle}
-        
-      />));
+        style={{marginLeft:'10px',marginRight:'10px'}}
+      >
+        {RoleName}
+       </Button>
+     ));
     }
   }
 return RenderRoles;
@@ -228,7 +260,7 @@ return RenderRoles;
         </Link>
       </MenuItem>
     )}
-      {role===PossibleRoles[2] && (
+      {(role===PossibleRoles[1]||role===PossibleRoles[2])  && (
       <MenuItem onClick={handleClose}>
         <Link href="/supporter-settings">
           <Typography component="h6" variant="h6">
@@ -240,13 +272,57 @@ return RenderRoles;
     
  
   <MenuItem onClick={handleModalOpen}>
-    
+  <Link >
       <Typography component="h6" variant="h6">
         Switch User
       </Typography>
+      </Link>
   </MenuItem>
-  <Dialog onClose={handleModalClose} aria-labelledby="customized-dialog-title" open={openModal}>
-  </Dialog>
+  {/* <Dialog onClose={handleModalClose} aria-labelledby="customized-dialog-title" open={openModal}>
+  </Dialog> */}
+   {/* <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={openModal}
+        onClose={handleModalClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={openModal}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Which role do you want to choose?</h2>
+      <p id="transition-modal-description" style={{textAlign:'center'}}>{renderRolesInModal()}</p>
+          </div>
+        </Fade>
+      </Modal> */}
+       <Dialog
+        open={openModal}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleModalClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">{"Which role do you want to choose?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+           Given below are the list of the roles that you have been registered as. Select the role you want to switch to.
+          </DialogContentText>
+          <div style={{textAlign:'center'}}>
+          {renderRolesInModal()}
+          </div>
+        </DialogContent>
+        <DialogActions>
+          
+          <Button className={classes.button} onClick={handleModalClose} >
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
   <MenuItem onClick={handleClose}>
     <Link href="/feedback">
       <Typography component="h6" variant="h6">
