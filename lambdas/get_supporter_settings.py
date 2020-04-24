762be260cac80264fae634fc7abe69d9747d415b
nhanfiction <nhan.le.7062@gmail.com>
2020-04-24 14:32:47 -0400

    Fixed problems with specializations list and nulls

--- lambdas/get_supporter_settings.py
+++ lambdas/get_supporter_settings.py
@@ -1,16 +1,15 @@
 #Written by Nhan Le
 
 from package.query_db import query
-from package.dictionary_to_list import dictionary_to_list
 import json
 
 #Function to update a supporter's settings
 #Inputs: supporter_id
 #Output: first_name, preferred_name, last_name, email, pronouns, gender, link, 
-#       phone, bio, picture, employer, title, team_name, professional_staff, student_staff,
-#       alumni, faculty, other, list of tags, list of specialization_types, 
-#       notification_type_name, feedback, list of supporter_preferences_for_student,
-#       list of appointment durations (for each specialization), max_students, google_doc_link, office, list of majors and major_id
+#        phone, bio, picture, employer, title, team_name, professional_staff, student_staff,
+#        alumni, faculty, other, list of tags, list of specialization_types, 
+#        notification_type_name, feedback, list of supporter_preferences_for_student,
+#        list of appointment durations (for each specialization), max_students, google_doc_link, office, list of majors and major_id
 # ^^^ fix
 
 def get_supporter_settings(event, context):
@@ -29,12 +28,13 @@ def get_supporter_settings(event, context):
 
     #Execute parameterized query to get user info
     sql = 'SELECT first_name, preferred_name, last_name, email, pronouns, gender, phone, \
-           bio, picture FROM users WHERE id = :supporter_id;'
+           bio, picture, employer, title, team_name, feedback, office, google_doc_link FROM users, supporters \
+           WHERE users.id = supporters.supporter_id AND users.id = :supporter_id;'
     sql_parameters = [{'name': 'supporter_id', 'value': {'longValue': supporter_id}}]
     settings = query(sql, sql_parameters)['records']
     if(settings == []):
         return {
-            'body': json.dumps("Users info could not be found"),
+            'body': json.dumps("User's info could not be found"),
             'statusCode': 404
         }
     block['first_name'] = settings[0][0].get('stringValue')
@@ -46,23 +46,12 @@ def get_supporter_settings(event, context):
     block['phone'] = settings[0][6].get('stringValue')
     block['bio'] = settings[0][7].get('stringValue')
     block['picture'] = settings[0][8].get('stringValue')
-
-    #Execute parameterized query to get supporter info
-    sql = 'SELECT employer, title, team_name, feedback, office, google_doc_link \
-           FROM supporters WHERE supporter_id = :supporter_id;'
-    sql_parameters = [{'name': 'supporter_id', 'value': {'longValue': supporter_id}}]
-    settings = query(sql, sql_parameters)['records']
-    if(settings == []):
-        return {
-            'body': json.dumps("Supporters info could not be found"),
-            'statusCode': 404
-        }
-    block['employer'] = settings[0][0].get('stringValue')
-    block['title'] = settings[0][1].get('stringValue')
-    block['team_name'] = settings[0][2].get('stringValue')
-    block['feedback'] = settings[0][3].get('stringValue')
-    block['office'] = settings[0][4].get('stringValue')
-    block['google_doc_link'] = settings[0][5].get('stringValue')
+    block['employer'] = settings[0][9].get('stringValue')
+    block['title'] = settings[0][10].get('stringValue')
+    block['team_name'] = settings[0][11].get('stringValue')
+    block['feedback'] = settings[0][12].get('stringValue')
+    block['office'] = settings[0][13].get('stringValue')
+    block['google_doc_link'] = settings[0][14].get('stringValue')
 
     #Execute parameterized query to get supporter types
     sql = 'SELECT professional_staff, student_staff, alumni, faculty, other \
@@ -94,14 +83,18 @@ def get_supporter_settings(event, context):
     specialization_types = [] #List of specialization types
     durations = [] #List of appointment durations and type
     for entry in specials:
-        temp_dict = {}
-        temp_dict['specialization_type'] = entry[2].get('stringValue')
-        specialization_types.append(temp_dict) 
-        temp_dict['duration'] = entry[1].get('longValue')
-        durations.append(temp_dict)
-    block['max_students'] = specials[0][0].get('longValue')
+        dur = {}
+        spec = {}
+        spec['specialization_type'] = entry[2].get('stringValue')
+        dur['specialization_type'] = entry[2].get('stringValue')
+        dur['duration'] = entry[1].get('longValue')
+        dur['max_students'] = entry[0].get('longValue')
+        if(spec not in specialization_types):
+            specialization_types.append(spec)
+        if(dur not in durations):
+            durations.append(dur)
     block['specialization_types'] = specialization_types
-    block['appointment_durations'] = durations
+    block['appointment_type_info'] = durations
 
     #Execute parameterized query to get supporter preferences for students
     sql = 'SELECT S1.grad_student, S1.job_search, S2.major_id, M.major \
@@ -128,36 +121,30 @@ def get_supporter_settings(event, context):
     sql = 'SELECT tags FROM tags WHERE supporter_id = :supporter_id;'
     sql_parameters = [{'name': 'supporter_id', 'value': {'longValue': supporter_id}}]
     settings = query(sql, sql_parameters)['records']
+    if(settings != []):
+        block['tags'] = settings[0][0].get('arrayValue')
     if(settings == []):
-        return {
-            'body': json.dumps("Tags could not be found"),
-            'statusCode': 404
-        }
-    block['tags'] = settings[0][0].get('arrayValue')
+        block['tags'] = None
 
     #Execute parameterized query to get tags
     sql = 'SELECT N2.notification_type_name FROM notification_preferences N1, notification_type N2 \
            WHERE N1.notification_type_id = N2.notification_type_id AND N1.user_id = :supporter_id;'
     sql_parameters = [{'name': 'supporter_id', 'value': {'longValue': supporter_id}}]
     settings = query(sql, sql_parameters)['records']
+    if(settings != []):
+        block['notification_type_name'] = settings[0][0].get('stringValue')
     if(settings == []):
-        return {
-            'body': json.dumps("Notification type could not be found"),
-            'statusCode': 404
-        }
-    block['notification_type_name'] = settings[0][0].get('stringValue')
+        block['notification_type_name'] = None
 
     #Execute parameterized query to get link
     sql = 'SELECT L2.link FROM user_link L1, link L2 \
            WHERE L1.link_id = L2.link_id AND L1.user_id = :supporter_id;'
     sql_parameters = [{'name': 'supporter_id', 'value': {'longValue': supporter_id}}]
     settings = query(sql, sql_parameters)['records']
+    if(settings != []):
+        block['link'] = settings[0][0].get('stringValue')
     if(settings == []):
-        return {
-            'body': json.dumps("Link could not be found"),
-            'statusCode': 404
-        }
-    block['link'] = settings[0][0].get('stringValue')
+        block['link'] = None
 
     return {
         'body': json.dumps(block),
