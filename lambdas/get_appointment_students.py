e879b1754eb1a1819d1ea4a9bf0f749fe04473b2
Matt Hill <mattjhill@umass.edu>
2020-04-29 13:05:45 -0400

    changed returns to reflect updated db

--- lambdas/get_appointment_students.py
+++ lambdas/get_appointment_students.py
@@ -4,7 +4,7 @@ from package.query_db import query
 #Written by Matt Hill
 #Input: student_id
 #Output: JSON object of current appointments that a student has in the format: 
-# "studentFN","studentLN","supporterFN","supporterLN","supporterPic","time_of_appt","type","duration","method","location","comment","feedback","rating"
+# "studentFN","studentLN","studentPic","supporterFN","supporterLN","supporterPic","time_of_appt","time_scheduled","medium","cancelled","cancel_reason","location","feedback","rating","comment","promoter_score","max_students","duration","specialization_type"
 
 def get_appointment_students(event, context):
     
@@ -22,9 +22,9 @@ def get_appointment_students(event, context):
         }
 
     #The user does exist, so fetch appointments
-    sql = 'SELECT U1.first_name AS studentFN, U1.last_name AS studentLN, U2.first_name AS supporterFN, U2.last_name AS supporterLN, U2.picture AS supporterPic, SA.time_of_appt, SA.type,SA.duration, SA.method, SA.location, SR.comment, SR.feedback, SR.rating \
-            FROM students S, users U1, users U2, student_appointment_relation SR, scheduled_appointments SA \
-             WHERE S.student_id = SR.student_id AND SR.appointment_id = SA.appointment_id AND S.student_id = U1.id AND SA.supporter_id = U2.id AND S.student_id=:given_id' 
+    sql = 'SELECT U1.first_name AS studentFN, U1.last_name AS studentLN, U1.picture AS studentPic, U2.first_name AS supporterFN, U2.last_name AS supporterLN, U2.picture AS supporterPic, SA.time_of_appt, SA.time_scheduled, SA.medium,SA.cancelled, SA.cancel_reason,SA.location, SR.feedback, SR.rating, SR.comment, SR.promoter_score, SS.max_students, SS.duration, ST.specialization_type \
+            FROM students S, users U1, users U2, student_appointment_relation SR, scheduled_appointments SA, supporter_specializations SS, specialization_type ST \
+             WHERE S.student_id = SR.student_id AND SR.appointment_id = SA.appointment_id AND S.student_id = U1.id AND SA.supporter_id = U2.id AND SA.supporter_id = SS.supporter_id AND ST.specialization_type_id = SS.specialization_type_id AND S.student_id=:given_id' 
     sql_parameters = [{'name':'given_id', 'value' : {'longValue': given_id}}]
     appointment_info = query(sql, sql_parameters)
 
@@ -42,17 +42,24 @@ def get_appointment_students(event, context):
             block = dict()
             block["studentFN"] = entry[0].get("stringValue")
             block["studentLN"] = entry[1].get("stringValue")
-            block["supporterFN"] = entry[2].get("stringValue")
-            block["supporterLN"] = entry[3].get("stringValue")
-            block["supporterPic"] = entry[4].get("stringValue")
-            block["time_scheduled"] = entry[5].get("stringValue")
-            block["type"] = entry[6].get("stringValue")
-            block["duration"] = entry[7].get("longValue")
-            block["method"] = entry[8].get("stringValue")
-            block["location"] = entry[9].get("stringValue")
-            block["comment"] = entry[10].get("stringValue")
-            block["feedback"] = entry[11].get("stringValue")
-            block["rating"] = entry[12].get("longValue")
+            block["studentPic"] = entry[2].get("stringValue")
+            block["supporterFN"] = entry[3].get("stringValue")
+            block["supporterLN"] = entry[4].get("stringValue")
+            block["supporterPic"] = entry[5].get("stringValue")
+            block["time_of_appt"] = entry[6].get("stringValue")
+            block["time_scheduled"] = entry[7].get("stringValue")
+            block["medium"] = entry[8].get("stringValue")
+            block["cancelled"] = entry[9].get("booleanValue")
+            block["cancel_reason"] = entry[10].get("stringValue")
+            block["location"] = entry[11].get("stringValue")
+            block["feedback"] = entry[12].get("stringValue")
+            block["rating"] = entry[13].get("longValue")
+            block["comment"] = entry[14].get("stringValue")
+            block["promoter_score"] = entry[15].get("booleanValue")
+            block["max_students"] = entry[16].get("longValue")
+            block["duration"] = entry[17].get("longValue")
+            block["specialization_type"] = entry[18].get("stringValue")
+
 
             student_appointments.append(block)
 
