0029f3c357fe8146bdefb278d998c12cf8ce3c6f
dduong0ff <dduong@umass.edu>
2020-04-28 20:34:31 -0400

    Added implementation

--- lambdas/upload_file.py
+++ lambdas/upload_file.py
@@ -3,8 +3,35 @@ import boto3
 
 # Written by Dat Duong
 
-# input: file
+# input: file_name
+
+print("OOF")
 
 
-def upload_file(event, context):
+def upload_files(event, context):
     print("hello")
+
+    # get the file name
+    file_name = event['file_name']
+
+    # s_3 bucket
+    bucket_name = 't1-s3-us-east-1'
+
+    # s_3 = boto3.client('s_3')
+    s_3 = boto3.resource('s_3')
+
+    # actually uploading
+    # response = s_3.Bucket(bucket_name).upload_file(file_name, file_name)
+    response = s_3.meta.client.upload_file(file_name, bucket_name, file_name)
+
+    # check if file was uploaded
+    if response['numberOfRecordsUpdated'] == 0:
+        return {
+            'statusCode': 404,
+            'body': json.dumps('Error uploading file')
+        }
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('File %s Uploaded!!!!' % file_name)
+    }
