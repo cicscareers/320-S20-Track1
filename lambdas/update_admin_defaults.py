609ac3bf5833a000645743fdef6a2ec453cd05b3
Matt Hill <mattjhill@umass.edu>
2020-05-01 13:25:27 -0400

    updated query to account for medium

--- lambdas/update_admin_defaults.py
+++ lambdas/update_admin_defaults.py
@@ -7,17 +7,17 @@ from package.query_db import query
 
 def update_admin_defaults(event,context):
 
-    medium = event['medium_of_appt']
-    max_students = event['max_students_for_specialization']
-    duration = event['duration_for_specialization']
-    hours = event['hours_before_appointment_for_supporter']
+    medium_name = event['medium_of_appt']
+    max_students = int(event['max_students_for_specialization'])
+    duration = int(event['duration_for_specialization'])
+    hours = int(event['hours_before_appointment_for_supporter'])
 
-    sql = 'UPDATE default_table SET medium_of_appt =:medium, \
+    sql = 'UPDATE default_table SET medium_of_appt =(SELECT medium_id FROM medium WHERE medium = :medium_name), \
     max_students_for_specialization =:max_students, \
     duration_for_specialization =:duration, \
     hours_before_appointment_for_supporter =:hours'
 
-    sql_parameters = [{'name':'medium', 'value' : {'longValue' : medium}},
+    sql_parameters = [{'name':'medium_name', 'value' : {'stringValue' : medium_name}},
                         {'name':'max_students', 'value' : {'longValue' : max_students}},
                         {'name':'duration', 'value' : {'longValue' : duration}},
                         {'name':'hours', 'value' : {'longValue' : hours}}]
