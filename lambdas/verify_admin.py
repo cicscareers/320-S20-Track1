c698a3d65eced7929a61b86d49cad7bbfbcce3db
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2020-05-01 14:08:46 -0700

    Added lambda exceptions

--- lambdas/verify_admin.py
+++ lambdas/verify_admin.py
@@ -1,31 +1,25 @@
 import json
 from package.query_db import query
 from package.dictionary_to_list import dictionary_to_list
+from package.lambda_exception import LambdaException
 
 def verify_admin(event, context):
     user_id = int(event['user_id'])
     user_id_dic = {}
     if user_id == None: #Making sure user_id was passed
-        return{
-            "statusCode": 404
-        }
+       return LambdaException("400: user_id was not given")
 
     user_id_dic['user_id'] = user_id
     sql_parameters = dictionary_to_list(user_id_dic)
     sql_select = """SELECT users.id FROM users WHERE users.id = :user_id""" #This query is ensuring that the user exists
     response = query(sql_select, sql_parameters)
     if response['records'] == []: #Returning error if user does not exist
-        print("User does not exist")
-        return{
-            "statusCode": 404
-        }
+       return LambdaException("404: user does not exist")
+
     sql_select = """SELECT users.id FROM users WHERE users.id = :user_id and is_admin = true""" #This query is ensuring user is not already an admin
     response = query(sql_select, sql_parameters)
     if response['records'] != []: #Returning error if user is already an admin
-        print("User is already an admin")
-        return{
-            "statusCode": 404
-        }
+        return LambdaException("405: user is already an admin")
     else:
         sql_update = """UPDATE users SET is_admin = true WHERE users.id = :user_id"""
         response = query(sql_update, sql_parameters)
