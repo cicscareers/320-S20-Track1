0751b3c407b645371b94f72087fcade440db5986
Matt Hill <mattjhill@umass.edu>
2020-05-01 16:09:00 -0400

    fixed rating field

--- lambdas/get_all_appointments.py
+++ lambdas/get_all_appointments.py
@@ -20,7 +20,7 @@ def get_all_appointments(event, context):
         }
     else:
         #There are appointments scheduled, so fetch appointments
-        sql = 'SELECT distinct U1.first_name AS studentFN, U1.last_name AS studentLN, U1.picture AS studentPic, U2.first_name AS supporterFN, U2.last_name AS supporterLN, U2.picture as supporterPic, SA.time_of_appt, SA.time_scheduled, SA.medium, SA.cancelled, SA.cancel_reason, SA.location, SR.feedback, SR.rating, SR.comment, SR.promoter_score, SS.max_students, SS.duration, ST.specialization_type, SR.appointment_id \
+        sql = 'SELECT distinct U1.first_name AS studentFN, U1.last_name AS studentLN, U1.picture AS studentPic, U2.first_name AS supporterFN, U2.last_name AS supporterLN, U2.picture AS supporterPic, SA.time_of_appt, SA.time_scheduled, SA.medium, SA.cancelled, SA.cancel_reason, SA.location, SR.feedback, SR.rating, SR.comment, SR.promoter_score, SS.max_students, SS.duration, ST.specialization_type, SR.appointment_id \
                 FROM users U1, users U2, student_appointment_relation SR, scheduled_appointments SA, supporter_specializations SS, specializations_for_appointment SFA, specialization_type ST \
                 WHERE SR.student_id = U1.id \
                 AND SR.supporter_id = U2.id \
@@ -43,15 +43,22 @@ def get_all_appointments(event, context):
             block["studentFN"] = entry[3].get("stringValue")
             block["studentLN"] = entry[4].get("stringValue")
             block["studentPic"] = entry[5].get("stringValue")
-            block["time_scheduled"] = entry[6].get("stringValue")
-            block["type"] = entry[7].get("stringValue")
-            block["duration"] = entry[8].get("longValue")
-            block["method"] = entry[9].get("stringValue")
-            block["location"] = entry[10].get("stringValue")
-            block["comment"] = entry[11].get("stringValue")
-            block["feedback"] = entry[12].get("booleanValue")
-            block["rating"] = entry[13].get("longValue")
-            block["promoter_score"] = entry[14].get("booleanValue")
+            block["time_of_appt"] = entry[6].get("stringValue")
+            block["time_scheduled"] = entry[7].get("stringValue")
+            block["medium"] = entry[8].get("stringValue")
+            block["cancelled"] = entry[9].get("booleanValue")
+            block["cancel_reason"] = entry[10].get("stringValue")
+            block["location"] = entry[11].get("stringValue")
+            block["feedback"] = entry[12].get("stringValue")
+            block["rating"] = entry[13].get("stringValue")
+            if block["rating"] is not None:
+                block["rating"] = float(block["rating"])
+            block["comment"] = entry[14].get("stringValue")
+            block["promoter_score"] = entry[15].get("booleanValue")
+            block["max_students"] = entry[16].get("longValue")
+            block["duration"] = entry[17].get("longValue")
+            block["specialization_type"] = entry[18].get("stringValue")
+            block["appointment_id"] = entry[19].get("longValue")
             appointments.append(block)
 
         #Returns the query contents
