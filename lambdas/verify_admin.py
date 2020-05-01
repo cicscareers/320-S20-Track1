faed515be7b09cdd5e97242f90f019cfb6ff48c4
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2020-05-01 14:25:09 -0700

    Fixed bug where return should be raise for lambda exception

--- lambdas/verify_admin.py
+++ lambdas/verify_admin.py
@@ -7,19 +7,19 @@ def verify_admin(event, context):
     user_id = int(event['user_id'])
     user_id_dic = {}
     if user_id == None: #Making sure user_id was passed
-       return LambdaException("400: user_id was not given")
+       raise LambdaException("400: user_id was not given")
 
     user_id_dic['user_id'] = user_id
     sql_parameters = dictionary_to_list(user_id_dic)
     sql_select = """SELECT users.id FROM users WHERE users.id = :user_id""" #This query is ensuring that the user exists
     response = query(sql_select, sql_parameters)
     if response['records'] == []: #Returning error if user does not exist
-       return LambdaException("404: user does not exist")
+       raise LambdaException("404: user does not exist")
 
     sql_select = """SELECT users.id FROM users WHERE users.id = :user_id and is_admin = true""" #This query is ensuring user is not already an admin
     response = query(sql_select, sql_parameters)
     if response['records'] != []: #Returning error if user is already an admin
-        return LambdaException("405: user is already an admin")
+        raise LambdaException("405: user is already an admin")
     else:
         sql_update = """UPDATE users SET is_admin = true WHERE users.id = :user_id"""
         response = query(sql_update, sql_parameters)
