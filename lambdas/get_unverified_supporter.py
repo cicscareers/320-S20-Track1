f8d77def0229324b0e72f599218f1c6fc627d7b3
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2020-05-01 15:25:46 -0700

    Added lambda exception and cleaned up code

--- lambdas/get_unverified_supporter.py
+++ lambdas/get_unverified_supporter.py
@@ -1,24 +1,21 @@
 import json
 from package.query_db import query
 from package.dictionary_to_list import dictionary_to_list
+from package.lambda_exception import LambdaException
+
 
 def get_unverified_supporters(event, context):
-    sql_parameter_dic = {}
-    sql_parameter_dic["val"] = 'true'
     sql_select ="""SELECT s.employer as s_employer, s.title as s_title, s.team_name as s_team_name, u.first_name as s_first_name, u.last_name as s_last_name, u.email as s_email, s.feedback as s_feedback, s.rating as s_rating, s.is_pending as s_is_pending, s.office as s_office, st.professional_staff as s_professional_staff, st.alumni as s_alumni, st.faculty as s_faculty, st.other as s_other, s.supporter_id as s_supporter_id, st.student_staff as s_student_staff\
                 FROM supporters s, users u, supporter_type st \
                 WHERE (s.is_pending = true or s.is_pending is NULL) AND s.user_id = u.id AND s.supporter_id = st.supporter_id;"""
 
-    sql_parameters = dictionary_to_list(sql_parameter_dic)
+    sql_parameters = dictionary_to_list({})
     response = query(sql_select, sql_parameters)
     if response['records'] == []:
-        print("No supporters")
-        return{
-            'statusCode': 404
-        }
+        raise LambdaException("404: there are no supporters") #Raising lambda exception if there are no supporters
     else:
         unverified_supporter_information = []
-        for entry in response['records']:
+        for entry in response['records']: #This for loop will append corrresponding data for JSON object
             block = dict()
             block["s_employer"] = entry[0].get("stringValue")
             block["s_title"] = entry[1].get("stringValue")
@@ -38,8 +35,8 @@ def get_unverified_supporters(event, context):
             block["s_student_staff"] = entry[15].get("booleanValue")
             unverified_supporter_information.append(block)
 
-    #Returns the query contents in JSON format
-        return{
+        #Returns the query contents in JSON format
+        return{ 
             'body': json.dumps(unverified_supporter_information),
             'statusCode': 200
         }
