a5e34c253609488a0ad0aa46d3c35c1e45a06bf4
Hadley Pope <hpope@umass.edu>
2020-04-30 20:50:49 -0400

    Gets all FAQs

---
+++ lambdas/get_faq.py
@@ -0,0 +1,27 @@
+from package.query_db import query
+from package.lambda_exception import LambdaException
+
+def handler(event, context):
+
+    get_faq_sql = "SELECT question, answer FROM FAQ;"
+
+    try:
+        faqs = query(get_faq_sql)['records']
+    except Exception as e:
+        raise LambdaException("500: Unable to get FAQs, " + str(e))
+    
+    response = {
+        'faqs' = [],
+    }
+
+    if len(faqs) > 0:
+        faqs = faqs[0]
+        for q, a in faqs:
+            curr_faqs = response['questions']
+            curr_faqs.append((q['stringValue'], a['stringValue']))
+            response['faqs'] = curr_faqs
+            
+    else:
+        raise LambdaException("204: No existing FAQs")
+
+    return response
