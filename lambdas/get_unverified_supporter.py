de0b5f1d025837a49c67969042c41d338ac42570
Sanjay Rajasekaran <rajasekaran.sanjay@gmail.com>
2020-04-30 19:59:06 -0700

    Fixed bugs

--- lambdas/get_unverified_supporter.py
+++ lambdas/get_unverified_supporter.py
@@ -4,16 +4,14 @@ from package.dictionary_to_list import dictionary_to_list
 
 def get_unverified_supporters(event, context):
     sql_parameter_dic = {}
-    sql_parameter_dic["val"] = 'false'
-    sql_select = """
-                SELECT s.employer as s_employer, s.title as s_title, s.team_name as s_team_name, u.first_name as s_first_name, u.last_name as s_last_name, u.email as s_email\
-                FROM supporter s, user u\
-                WHERE s.is_pending = :val AND s.user_id = u.id\
-                """
+    sql_parameter_dic["val"] = 'true'
+    sql_select ="""SELECT s.employer as s_employer, s.title as s_title, s.team_name as s_team_name, u.first_name as s_first_name, u.last_name as s_last_name, u.email as s_email \
+                FROM supporters s, users u \
+                WHERE s.is_pending = true AND s.user_id = u.id;"""
 
     sql_parameters = dictionary_to_list(sql_parameter_dic)
-    response = query(sql, sql_parameters)
-    if response['record'] == []:
+    response = query(sql_select, sql_parameters)
+    if response['records'] == []:
         print("No supporters")
         return{
             'statusCode': 404
@@ -28,10 +26,11 @@ def get_unverified_supporters(event, context):
             block["s_first_name"] = entry[3].get("stringValue")
             block["s_last_name"] = entry[4].get("stringValue")
             block["s_email"] = entry[5].get("stringValue")
+            unverified_supporter_information.append(block)
 
     #Returns the query contents in JSON format
         return{
-            'body': json.dumps(student_appointments),
+            'body': json.dumps(unverified_supporter_information),
             'statusCode': 200
         }
     
