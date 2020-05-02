7454efe22df3d6920cf21b4bfa007281613c6f71
dduong0ff <dduong@umass.edu>
2020-05-02 15:51:02 -0400

    updates the db with the file

--- lambdas/upload_file.py
+++ lambdas/upload_file.py
@@ -2,6 +2,7 @@ import json
 import boto3
 import mimetypes
 from package.lambda_exception import LambdaException
+from package.query_db import query
 from datetime import datetime
 
 # Written by Dat Duong
@@ -44,18 +45,47 @@ def upload_files(event, context):
 
         bucket_name = bucket_name_resumes
 
+    # new file name going into s3
+    key = str(user_id) + '/' + now + '_' + file_name
+
     try:
         # actually uploading
         # creating/adding user_id folder and adding timestamp to file_name
+        # with open(file_name, 'rb') as data:
+        #     s_3.upload_fileobj(data, bucket_name, str(
+        #         user_id) + '/' + now + '_' + file_name)
+
         with open(file_name, 'rb') as data:
-            s_3.upload_fileobj(data, bucket_name, str(
-                user_id) + '/' + now + '_' + file_name)
+            s_3.upload_fileobj(data, bucket_name, key)
 
         # response = s_3.meta.client.upload_file(
         #     file_name, bucket_name, file_name)
 
     except Exception as e:
         raise LambdaException("400: File failed to uploaded")
+
+    # maybe update in database
+
+    if file_type == 'picture':
+
+        sql = 'UPDATE users SET picture = :picture WHERE id = :user_id'
+        sql_parameters = [{'name': 'picture', 'value': {'stringValue': key}},
+                          {'name': 'user_id', 'value': {'longValue': user_id}}]
+
+    if file_type == 'resume':
+
+        sql = 'UPDATE students SET resume = :resume from users WHERE users.id = :user_id and users.id = students.user_id'
+        sql_parameters = [{'name': 'resume', 'value': {'stringValue': key}},
+                          {'name': 'user_id', 'value': {'longValue': user_id}}]
+
+    update_query = query(sql, sql_parameters)
+
+    # Check if updated
+    if(update_query['numberOfRecordsUpdated'] == 0):
+        return{
+            'body': json.dumps("file not in database"),
+            'statusCode': 500
+        }
 
     return {
         'statusCode': 200,
