f76746ba8e3e20e6fdca459bd9b98680626800d3
dduong0ff <dduong@umass.edu>
2020-05-01 23:44:56 -0400

    minor updates and comments

--- lambdas/upload_file.py
+++ lambdas/upload_file.py
@@ -10,7 +10,6 @@ from datetime import datetime
 # input: user_id
 # input: file_type ??????????????????
 
-
 print("OOF")
 
 
@@ -47,12 +46,13 @@ def upload_files(event, context):
 
     try:
         # actually uploading
+        # creating/adding user_id folder and adding timestamp to file_name
         with open(file_name, 'rb') as data:
             s_3.upload_fileobj(data, bucket_name, str(
                 user_id) + '/' + now + '_' + file_name)
 
         # response = s_3.meta.client.upload_file(
-        #     file_name, bucket_name, "gen2_folder/" + file_name) # maybe timestamp
+        #     file_name, bucket_name, file_name)
 
     except Exception as e:
         raise LambdaException("400: File failed to uploaded")
