385997b56d09d5f917a79bdcf813666115fb7f15
Sean Powell <powellsl99@gmail.com>
2020-06-03 18:44:29 -0400

    add files git deleted

---
+++ Client/src/Pages/TOS.js
@@ -0,0 +1,23 @@
+  
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
+
+export default function TOS() {
+  return (
+    <Container component="main" maxWidth="xs" align="center">
+      <Typography component="h1" variant="h5" align="center">
+        Terms and Conditions
+      </Typography>
+      <Grid container align="center">
+        <Grid item xs>
+          <Link href="/signup" variant="body2" align="center">
+            Back to sign up
+          </Link>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
