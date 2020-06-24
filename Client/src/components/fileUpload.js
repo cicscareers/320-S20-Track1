eec1412f3ef7c0b68cc7d369b60b94b8f9d509ac
ishchhabra <ishchhabra12@gmail.com>
2020-06-24 20:48:56 +0530

    Added onFileChange prop
    Added onFileChange prop as well as hid the ugly input tag.
    Also added state variable file to show the file name in the button since the input tag is hidden now.
    
    First time writing React component so please check if it contains some bug.

--- Client/src/components/fileUpload.js
+++ Client/src/components/fileUpload.js
@@ -1,22 +1,37 @@
 
-import React from 'react';
+import React, {Component, setState, props} from 'react';
 import {Button} from '@material-ui/core';
 
-export default function fileUpload() {
-    return (
-        <Button
-            variant="contained"
-            margin="normal"
-            component="label"
-            fullWidth
-            align='center'
-            color='secondary'
-        >
-            Upload a Profile Picture: &nbsp;&nbsp;
+export default class fileUpload extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {file: ""};
+        this.onChangeFile = this.onChangeFile.bind(this);
+    }
+
+    render() {
+        return (
+            <Button
+                variant="contained"
+                margin="normal"
+                component="label"
+                fullWidth
+                align='center'
+                color='secondary'
+            >
+            Upload a Profile Picture: &nbsp;&nbsp; {this.state.file}
             <input
                 type="file"
                 fullWidth
                 align='right'
+                onChange={this.onChangeFile}
+                style={{display: 'none'}}
             />
-        </Button>)
+            </Button>)
+    }
+
+    onChangeFile(e) {
+        this.setState({file: e.target.value});
+        this.props.onFileChange();
+    }
 }
