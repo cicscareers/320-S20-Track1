c357ffa8fc86664603903e1f060ce7582edf6a82
ishchhabra <ishchhabra12@gmail.com>
2020-06-26 21:26:47 +0530

    Added amplify configuration

--- MobileClient/index.js
+++ MobileClient/index.js
@@ -5,5 +5,15 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {Amplify} from 'aws-amplify';
 
 AppRegistry.registerComponent(appName, () => App);
+
+Amplify.configure({
+    Auth: {
+      mandatorySignIn: true,
+      region: 'us-east-2',
+      userPoolId: 'us-east-2_TOeWJwIy0',
+      userPoolWebClientId: '7phnpqt6kfvfr9apoelke4hhm1'
+    }
+  });
