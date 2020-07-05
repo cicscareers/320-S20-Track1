a569d69c8539b067ecb50b59f2ddbd23fb4977a8
ishchhabra <ishchhabra12@gmail.com>
2020-07-05 19:31:42 +0530

    Minor changes

--- lambdas/package/s3_utils.py
+++ lambdas/package/s3_utils.py
@@ -2,13 +2,11 @@ from package.query_db import query
 from package.lambda_exception import LambdaException
 import boto3
 
-
 def delete_file(bucket, path):
-    s_3 = boto3.client('s3')
     try:
-        s_3.delete_object(Bucket=bucket, Key=path)
+        boto3.client('s3').delete_object(Bucket=bucket, Key=path)
     except Exception as e:
-        raise LambdaException(f"Unable to delete file {path} from S3 bucket {bucket}: {str(e)}")
+        raise LambdaException(f"Unable to delete file {path} from S3 bucket {bucket}: {e}")
 
 bucket_name_images = 't1-s3-us-east-1-images' # s3 bucket for images
 def delete_image(path):
