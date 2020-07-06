6370a6c79ec4b3405e0483d7d47be51ec30ea078
ishchhabra <ishchhabra12@gmail.com>
2020-07-06 21:38:10 +0530

    Bug fixes + use transactions

--- lambdas/delete_user.py
+++ lambdas/delete_user.py
@@ -1,6 +1,7 @@
 import boto3
-from package.query_db import query
+from package.query_db import query, beginTransaction, commitTransaction
 from package.lambda_exception import LambdaException
+from package.dictionary_to_list import dictionary_to_list
 from package.db_utils import is_user_admin, user_exists, get_user_roles
 from package.s3_utils import delete_image
 
@@ -39,26 +40,23 @@ adminSet = [
 # {'longValue' : 'user_id'}
 def delete_user(event, context):
     admin_id = int(event['admin_id'])
-    admin_id_param = [{'name' : 'admin_id', 'value' : {'longValue' : admin_id}}]
-    
     user_id = int(event['user_id'])
-    user_id_param = [{'name' : 'user_id', 'value' : {'longValue' : user_id}}]
 
     if(not user_exists(user_id)):
         return {
-            'body': user_id + ' does not exist',
+            'body': str(user_id) + ' does not exist',
             'statusCode': 404
         }
     
-    if(not user_exists(admin_id_param)):
+    if(not user_exists(admin_id)):
         return {
-            'body': admin_id + ' does not exist',
+            'body': str(admin_id) + ' does not exist',
             'statusCode': 404
         }
 
-    if(not is_user_admin(admin_id_param)):
+    if(not is_user_admin(admin_id)):
         return {
-            'body': admin_id + ' is not admin',
+            'body': str(admin_id) + ' is not admin',
             'statusCode': 403
         }
     
@@ -69,7 +67,7 @@ def delete_user(event, context):
     tableSet = []
     tableSet.extend(userSet)
 
-    user_roles = get_user_roles(user_id_param)
+    user_roles = get_user_roles(user_id)
 
     if user_roles['isStudent']:
         tableSet.extend(studentSet)
@@ -79,11 +77,11 @@ def delete_user(event, context):
 
     if user_roles['isAdmin']:
         tableSet.extend(adminSet)
-
+    
     # Loop through tableSet and delete user_id
+    tranasactionID = beginTransaction()['transactionId']
+    user_id_param = [{'name' : 'user_id', 'value' : {'longValue' : user_id}}]
     for table in tableSet:
         sql = f"DELETE FROM {table[0]} WHERE {table[1]} = :user_id"
-        try:
-            query(sql, user_id_param)['records'][0]
-        except Exception as e:
-            raise LambdaException(f"Unable to delete user from '{table[0]}' table: {e}")
+        query(sql, user_id_param, tranasactionID)
+    commitTransaction(tranasactionID)
