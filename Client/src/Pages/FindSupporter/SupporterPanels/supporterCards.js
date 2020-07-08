a16883f23d6f2b1bb64eea65112890f786560d34
Sahil Joshi <52909523+sahiljoshi515@users.noreply.github.com>
2020-07-08 13:40:50 +0530

    Make Help Needed Topic consistent

--- Client/src/Pages/FindSupporter/SupporterPanels/supporterCards.js
+++ Client/src/Pages/FindSupporter/SupporterPanels/supporterCards.js
@@ -240,7 +240,7 @@ const SupporterCard = (props) => {
               <Typography>{employer}, {title}</Typography>
               <Typography>{office}</Typography>
               <br/>
-              <Typography>Select Appointment Topic:</Typography>
+              <Typography>Help Needed Topic:</Typography>
               {topics_array.map(topic => <Chip 
                 clickable 
                 value={topic}
