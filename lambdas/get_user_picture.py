156771b10b495ae2a0a59c118733ceca121d0178
ishchhabra <ishchhabra12@gmail.com>
2020-08-01 11:01:56 +0530

    Update get_user_picture.py

--- lambdas/get_user_picture.py
+++ lambdas/get_user_picture.py
@@ -1,23 +1,6 @@
-import boto3
-from package.lambda_exception import LambdaException
+from package.Users import Users
 
+# Returns the presigned url for the image.
 def get_picture(event, context):
-    id = int(event['id'])
-    response = {}
-    response["picture"] = get_profile_picture(id)
-    return response
-    
-def get_profile_picture(student_id):
-    s_3 = boto3.client('s3')
-    bucket_name_images = 't1-s3-us-east-1-images' # s3 bucket for images
-    file_path = 'profile/' + str(student_id) + '/image'
-
-    try:
-        response = s_3.get_object(Bucket=bucket_name_images, Key=file_path)
-    except Exception as e:
-        if(e.response['Error']['Code'] == "NoSuchKey"):
-            return ""
-        else:
-            raise LambdaException("400: Failed to download file." + str(e))
-
-    return response['Body'].read().decode('utf-8')
+    user_id = int(event['id'])
+    return Users.get_profile([user_id])[user_id]
