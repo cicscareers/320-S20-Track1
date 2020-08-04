5932d1f7cd1a6ff910fd2f54f4199519049c0233
ishchhabra <ishchhabra12@gmail.com>
2020-08-04 17:19:25 +0530

    Properly implement image conversion

--- Client/src/Pages/Settings Pages/StudentSettings/ProfileSettings/ConfirmImageDialog.js
+++ Client/src/Pages/Settings Pages/StudentSettings/ProfileSettings/ConfirmImageDialog.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Dialog, DialogTitle, DialogContent, DialogActions, Button} from '@material-ui/core'
+import {Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography, CircularProgress} from '@material-ui/core'
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 
@@ -8,32 +8,31 @@ export default class ConfirmImageModal extends React.Component {
         super(props);
         this.state = {
             open: false,
-            picture: props.picture,
-            
+            picture: null,
+            loading: false
         }
-
-        this.canvasRef = React.createRef();
     }
 
     toggleOpen = () => {
         this.setState({ open: !this.state.open });
     }
 
-    handleProfileChange(file) {
-        var fr = new FileReader();
-        fr.onload = (e) => {
-            this.setState({
-                open: true,
-                picture: e.target.result
-            })
-        }
-        fr.readAsDataURL(file);
+    async handleProfileChange(file) {
+        this.setState({
+            open: true, // Better to let the user know that image is being processed.
+            loading: true
+        });
+
+        file = await this.convertToJPEG(file);
+        
+        this.setState({
+            loading: false,
+            picture: file
+        });
     }
 
     handleImageUpload = () => {
-        this.canvasRef.current.toBlob((blob) => {
-            this.fetchPresignedPostURL(blob);
-        }, 'image/jpeg', 1.0);
+        this.fetchPresignedPostURL(this.state.picture);
     }
 
     fetchPresignedPostURL = (file) => {
@@ -49,27 +48,74 @@ export default class ConfirmImageModal extends React.Component {
                 method: 'POST',
                 body: formData
             })
-            .then((response) => response.json())
+            // .then((response) => response.json()) 
             .then((json) => console.log(json))
             .catch((error) => console.log(error))
         })
     }
 
+    convertToJPEG = (file) => {
+        return new Promise((resolve) => {
+            var canvas = document.createElement('canvas');
+            var ctx = canvas.getContext('2d');
+            var image = new Image();
+            image.onload = (e) => {
+                canvas.width = e.target.width;
+                canvas.height = e.target.height;
+                drawImage(e.target);
+            }
+            image.src = window.URL.createObjectURL(file);
+
+            function drawImage(image) {
+                var ctx = canvas.getContext('2d');
+                ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+                convertToJPEG();
+            }
+
+            var convertToJPEG = () => {
+                canvas.toBlob((blob) => {
+                    console.log(blob);
+                    resolve(blob);
+                }, 'image/jpeg', 1.0);
+            }
+        })
+    }
+
+    // saveBlobAs(blob, )
+
     render() {
+        if (this.state.loading) {
+            return (
+                <div align="center">
+                <br></br>
+                <Typography variant="h4">Loading...</Typography>
+                <br></br>
+                <CircularProgress />
+                </div>
+            )
+        }
+
         return (
-            <Dialog open={this.state.open} onClose={this.toggleOpen}>
+            <Dialog
+                open={this.state.open}
+                onClose={this.toggleOpen}
+                fullWidth
+                maxWidth={'md'}
+            >
                 <DialogTitle onClose={this.toggleOpen}>
                     Change Profile Picture
                     <IconButton onClick={this.toggleOpen} style={{position: 'absolute', right: '0px', top: '0px' }}>
                         <CloseIcon />
                     </IconButton>
                 </DialogTitle>
-                <DialogContent dividers>
-                    {/* Canvas is used for image conversion to JPEG. */}
-                    <canvas ref={this.canvasRef} width="200" height="100">
-                        {/* Please add styling here @Sahil. */}
-                        <img src={this.state.picture} />
-                    </canvas>
+                <DialogContent
+                    dividers
+                >
+                    <img
+                        src={this.state.picture ? window.URL.createObjectURL(this.state.picture) : ""}
+                        // Please add styling here.
+                    />
                 </DialogContent>
                 <DialogActions>
                     <Button autoFocus onClick={this.handleImageUpload} color="primary">
