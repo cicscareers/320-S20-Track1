d06be85f0bfb929e37c5bf87f33cedafc986d1ba
Sahil Joshi <52909523+sahiljoshi515@users.noreply.github.com>
2020-08-07 12:21:57 +0530

    Update NotFound.js

--- Client/src/Pages/NotFound.js
+++ Client/src/Pages/NotFound.js
@@ -3,18 +3,21 @@ import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
+import {withTranslation} from 'react-i18next';
 
-export default function notFound() {
+function notFound({t, i18n}) {
   return (
     <Container component="main" maxWidth="xs" align="center">
       <br/>
       <br/>
       <br/>
       <Typography component="h1" variant="h5" align="center">
-        We are sorry that we couldn't find the page. Please try again.
+        {t('page-not-found')}
       </Typography>
       <br/>
       <img  height="200" width="200" src ="cicscareers_logo_3.png"></img>
     </Container>
   );
 }
+
+export default withTranslation('errors')(notFound);
