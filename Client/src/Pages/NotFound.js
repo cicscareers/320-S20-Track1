758d354bc1b2780869e6c08d7bf23c4a4fd75e51
Sahil Joshi <52909523+sahiljoshi515@users.noreply.github.com>
2020-08-07 12:49:06 +0530

    Use string internalization

--- Client/src/Pages/NotFound.js
+++ Client/src/Pages/NotFound.js
@@ -3,19 +3,21 @@ import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
-import {withTranslation} from 'react-i18next';
+import { withTranslation } from 'react-i18next';
 
-function notFound({t, i18n}) {
+function notFound({ t, i18n }) {
   return (
     <Container component="main" maxWidth="xs" align="center">
-      <br/>
-      <br/>
-      <br/>
+      <br />
+      <br />
+      <br />
+      <img height="200" width="200" src="../cicscareers_logo_3.png" alt="Reachout Logo" />
+      <br />
+      <br />
       <Typography component="h1" variant="h5" align="center">
-        {t('page-not-found')}
+        <h1>{t('page-not-found.1')}</h1>
+        {t('page-not-found.2')}
       </Typography>
-      <br/>
-      <img  height="200" width="200" src ="cicscareers_logo_3.png"></img>
     </Container>
   );
 }
